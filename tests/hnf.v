845ff4f8a880fee08b05386fbac76ca1d39c98c7
Yves Bertot <Yves.Bertot@inria.fr>
2023-03-28 09:35:55 +0200

    hnf.v is not robust to change of indexing, but the Print command does not help

--- tests/hnf.v
+++ tests/hnf.v
@@ -8,11 +8,24 @@ HB.builders Context T of f T.
 HB.instance Definition _ := M.Build T (y + 1).
 HB.end.
 
+Search "HB_unnamed".
+(* This shows:
+  Builders_5.HB_unnamed_factory_7
+HB_unnamed_factory_2 *)
+
 #[hnf] HB.instance Definition _ := f.Build nat (3 + 2).
+Search "HB_unnamed".
+(* This shows:
+  HB_unnamed_factory_9
+  HB_unnamed_mixin_11
+*)
+Search "Datatypes_nat".
 Print Datatypes_nat__canonical__hnf_S.
-Print HB_unnamed_mixin_12.
+Print HB_unnamed_mixin_11.
 
 HB.instance Definition _ := f.Build bool (3 + 2).
 Print Datatypes_bool__canonical__hnf_S.
-Print HB_unnamed_mixin_16.
+Search "HB_unnamed_mixin".
+(* There is no other unnamed mixin here apart from mixin_11 *)
+(* Print HB_unnamed_mixin_16. *)
 
