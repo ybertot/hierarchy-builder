4608c53690dce9c8093c8d7ad1d85864948af63d
qvermande <quentin.vermande@ens.psl.eu>
2023-11-15 17:21:42 +0100

    declared sort and generic_type as coercions in HB/structures.v

--- structures.v
+++ structures.v
@@ -15,7 +15,7 @@ Definition ignore {T} (x: T) := x.
 Definition ignore_disabled {T T'} (x : T) (x' : T') := x'.
 
 (* ********************* structures ****************************** *)
-From elpi Require Import elpi.
+From elpi Require Import elpi coercion.
 
 Register unify as hb.unify.
 Register id_phant as hb.id.
@@ -619,6 +619,7 @@ HB.structure Definition StructureName params :=
 *)
 
 #[arguments(raw)] Elpi Command HB.structure.
+Elpi Accumulate Db coercion.db.
 Elpi Accumulate Db hb.db.
 Elpi Accumulate File "HB/common/stdpp.elpi".
 Elpi Accumulate File "HB/common/database.elpi".
